24c12142a7b7bfe44515f96601179b8a356efdf7
Michael Arakilian <arakilian0@gmail.com>
2019-09-04 08:34:39 -0700

    v0.4.1 release

--- config/tasks/default/default.js
+++ config/tasks/default/default.js
@@ -7,6 +7,7 @@ module.exports = function (cb) {
   print(chalk.bold('  create') + '       ' + chalk.gray('Create new resources for the project.'));
   print(chalk.bold('  build') + '        ' + chalk.gray('Process src/ files for distribution.'));
   print(chalk.bold('  runserver') + '    ' + chalk.gray('Local development server using BrowserSync.'));
+  print(chalk.bold('  deploy') + '       ' + chalk.gray('Push changes to pre configured AWS services.'));
 	print('');
 	cb();
 };
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ const _default = require('./config/tasks/default/default');
 const _create = require('./config/tasks/create/create');
 const _build = require('./config/tasks/build/build');
 const _runserver = require('./config/tasks/runserver/runserver');
+const _deploy = require('./config/tasks/deploy/deploy');
 
 exports.default = _default;
 exports.create = _create;
 exports.build = _build;
 exports.runserver = _runserver;
+exports.deploy = _deploy;
